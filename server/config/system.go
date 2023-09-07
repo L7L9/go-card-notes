ac09f12333a22e17ad441cb7cc865050b69ea7fc
lql <1964757607@qq.com>
2023-09-07 20:04:14 +0800

    feat(config):添加系统路由前缀的配置

--- server/config/system.go
+++ server/config/system.go
@@ -4,4 +4,5 @@ type SystemConfig struct {
 	Addr          int    `mapstructure:"addr"`
 	AdminAccount  string `mapstructure:"admin-account"`
 	AdminPassword string `mapstructure:"admin-password"`
+	RouterPrefix  string `mapstructure:"router-prefix"`
 }
