812269eb4b7b8ffa658d70340fe1622992cddc9b
lql <1964757607@qq.com>
2023-09-14 10:11:02 +0800

    feat(request):添加登录请求

--- server/model/common/request/user.go
+++ server/model/common/request/user.go
@@ -8,3 +8,10 @@ type SignUpRequest struct {
 	Nickname string `json:"nickname" binding:"required"`
 	Phone    string `json:"phone" binding:"required"`
 }
+
+// SignInRequest //
+// 登录请求
+type SignInRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
