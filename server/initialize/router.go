a6ed7abddb7a70fea815c7c60552410dd4d5e52f
lql <1964757607@qq.com>
2023-09-16 16:00:17 +0800

    feat(initialize):初始化路由添加后缀v1

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -11,16 +11,18 @@ import (
 // initialize router and group
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+	// 设置前缀
+	prefixGroup := r.Group(global.GCN_CONFIG.System.RouterPrefix)
 
 	// 不做鉴权的路由组
-	publicGroup := r.Group(global.GCN_CONFIG.System.RouterPrefix)
+	publicGroup := prefixGroup.Group("/v1")
 	{
 		// 初始化基础路由
 		router.RouterGroupOuter.BaseRouter.Initialize(publicGroup)
 	}
 
 	// 要做鉴权的路由组
-	privateGroup := r.Group(global.GCN_CONFIG.System.RouterPrefix, middleware.JwtAuth(), middleware.CasbinAuth())
+	privateGroup := prefixGroup.Group("/v1", middleware.JwtAuth(), middleware.CasbinAuth())
 	{
 		// 初始化路由
 		router.RouterGroupOuter.UserRouter.Initialize(privateGroup)
