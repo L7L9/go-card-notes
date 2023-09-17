1287ce13c6e0619f0c9d45f30cae494359b1bfd6
lql <1964757607@qq.com>
2023-09-17 16:47:15 +0800

    feat(schema):添加标签表

---
+++ server/model/schema/tag.go
@@ -0,0 +1,14 @@
+package schema
+
+import "gorm.io/gorm"
+
+// Tag //
+// 标签
+type Tag struct {
+	gorm.Model
+	Name string `gorm:"NOT NULL;comment:'标签名'"`
+}
+
+func (Tag) TableName() string {
+	return "gcn_tag"
+}
