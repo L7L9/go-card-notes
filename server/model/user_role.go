e6f815743e51941b09889e96e56a901fb7b59572
lql <1964757607@qq.com>
2023-10-13 12:06:20 +0800

    refactor(model):角色表添加ID

--- server/model/user_role.go
+++ server/model/user_role.go
@@ -3,6 +3,7 @@ package model
 // UserRole //
 // gcn_user_role table
 type UserRole struct {
+	ID     uint `gorm:"primarykey"`
 	UserID uint `gorm:"column:user_id"`
 	RoleID uint `gorm:"column:role_id"`
 }
