f6e427bc053549ea7089160e56fb785c341e727a
lql <1964757607@qq.com>
2023-10-15 11:52:30 +0800

    feat(router):添加note创建分组的路由

--- server/router/note.go
+++ server/router/note.go
@@ -1,6 +1,9 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	v1 "lqlzzz/go-card-notes/api/v1"
+)
 
 type NoteRouter struct {
 }
@@ -8,8 +11,11 @@ type NoteRouter struct {
 // Initialize //
 // 初始化note组路由
 func (router *NoteRouter) Initialize(fatherGroup *gin.RouterGroup) {
+	noteApi := v1.ApiOuter.NoteApi
+
 	noteRouter := fatherGroup.Group("/note")
 	{
-		noteRouter.POST("/createNote/")
+		noteRouter.POST("/createNote/", noteApi.CreateNote)
+		noteRouter.POST("/createNoteGroup/", noteApi.CreateNoteGroup)
 	}
 }
