ed5a30628ca2c352072ef81c037f85ddc5a27b78
lql <1964757607@qq.com>
2023-10-15 11:50:15 +0800

    feat(request):添加创建笔记分组请求

--- server/model/common/request/note.go
+++ server/model/common/request/note.go
@@ -1,12 +1,16 @@
 package request
 
-import "mime/multipart"
-
 // CreateNoteRequest //
 // 创建笔记请求
 type CreateNoteRequest struct {
-	Title    string                `form:"title" json:"title"`
-	TextFile *multipart.FileHeader `form:"textFile" json:"textFile"`
-	Tags     []string              `form:"tags" json:"tags" `
-	Status   int                   `form:"status" json:"status"`
+	Title  string `form:"title" json:"title"`
+	Group  string `form:"Group" json:"group"`
+	Status int    `form:"status" json:"status"`
+}
+
+// CreateNoteGroupRequest //
+// 创建笔记分组
+type CreateNoteGroupRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
